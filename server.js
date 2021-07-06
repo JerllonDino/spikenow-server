3cd06af98e66f0eded4bd721be6a9f361be54443
JerllonDino <jerllon03@gmail.com>
2021-07-07 01:16:11 +0800

    added cors origin
    Signed-off-by: JerllonDino <jerllon03@gmail.com>

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use(
   session({ secret: "spikenowsecret", saveUninitialized: false, resave: false })
